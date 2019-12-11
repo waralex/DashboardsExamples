74492597e9ac05ae4b202af6015a209cbc34a56f
TheComputerM <mudit.harshit99@outlook.com>
2019-12-11 16:40:58 +0530

    Added a new example class data

--- Examples/Class_Data.jl
+++ Examples/Class_Data.jl
@@ -2,22 +2,22 @@
 import HTTP, CSV, JSON
 using Dashboards, PlotlyBase, DataFrames
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
-df = CSV.read(HTTP.get("https://people.sc.fsu.edu/~jburkardt/data/csv/mlb_teams_2012.csv").body)
+df = CSV.read(HTTP.get("https://filebin.net/og3ajfopziv4453x/Class_Data.csv?t=7et6ovzg").body)
 app = Dash("Dash Layout", external_stylesheets=external_stylesheets) do
     html_div() do
         dcc_graph(figure = Plot(df,
                     Layout(xaxis_type = "log",
-                        xaxis_title = "Payroll (millions)",
-                        yaxis_title = "Wins",
+                        xaxis_title = "Study",
+                        yaxis_title = "Marks",
                         legend_x = 0,
                         legend_y = 1,
                         hovermode = "closest"),
-                    x=Symbol("Payroll (millions"),
-                    y=Symbol("Wins"),
-                    text = :Team,
+                    x=Symbol("Study"),
+                    y=Symbol("Marks"),
+                    text = :Name,
                     mode="markers",
-                    group=:Wins,
-                    marker_size = 30,
+                    group=:Gender,
+                    marker_size = 10,
                     marker_line_width = 1,
                     marker_line_color = "blue"
                     ))
@@ -25,5 +25,5 @@ app = Dash("Dash Layout", external_stylesheets=external_stylesheets) do
 end
 
 handler = make_handler(app, debug = true)
-println("Started at localhost:8000")
-HTTP.serve(handler, HTTP.Sockets.localhost, 8000)
+println("Started at localhost:8080")
+HTTP.serve(handler, HTTP.Sockets.localhost, 8080)
