0d27e7e6d5eaef17edd464dbae250dfd789c4179
Madhav Shekhar Sharma <33770878+PseudoCodeNerd@users.noreply.github.com>
2019-12-29 18:32:14 +0530

    call_back for getting data into pieplot still left

--- Examples/pieplot_dash.jl
+++ Examples/pieplot_dash.jl
@@ -6,51 +6,49 @@ using Dashboards, PlotlyJS, DataFrames, HTTP, CSV, JSON, DataFramesMeta
 df = CSV.read(HTTP.get("https://raw.githubusercontent.com/plotly/datasets/master/2014_ebola.csv").body)
 df = dropmissing(df)
 
-external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css","https://codepen.io/ihackyouridevice/pen/eYmEjOP.css"]
+external_stylesheets = ["https://codepen.io/ihackyouridevice/pen/povrOWv.css","https://codepen.io/ihackyouridevice/pen/Jjoyary.css"]
 
+palette = (text = "#222f3e", bg = "#ffffff")
 
 #Constructing Dashboard Application
-app = Dash("Ebola Cases in Africa : PiePlot", external_stylesheets=external_stylesheets) do 
-    html_div() do 
-        #heading 
+app = Dash("Dashboard.jl", external_stylesheets=external_stylesheets) do
+    html_div(style = (backgroundColor = palette.bg, height = "100%")) do
+        #Sets and styles heading/title
         html_h1("Ebola Cases Reported in Africa - 2014",
-        style=(
-                textAlign = "center",
-                color = "#2d3436",
-                paddingBottom = "0%",
+            style=(
+               textAlign = "center",
+               paddingBotton = "0%",
+               color = palette.text,
+               fontFamily = "Helvetica",
+               fontSize = "82",
             )
-        )
-        #actual graph
-        html_div(className = "container", style = (textAlign = "center", margin ="30px", padding = "10px", width="65%", marginLeft="auto", marginRight="auto")) do 
-            dcc_graph(id = "my-graph"),
-            dcc_slider(
-                id = "month-selected",
-                min = 3,
-                max = 12,
-                marks = Dict(3 => "March", 4 => "April", 5 => "May", 6 => "June", 7 => "July", 8 => "August", 9 => "September", 10 => "October", 11 => "November", 12 => "December"),
-
-                value = 8,
+        ),
+        html_div(className = "container", style = (textAlign="center", margin="30px", padding="10px", width="65%", marginLeft="auto", marginRight="auto")) do
+            dcc_graph(
+               id = "my-graph"
+            ),
+           dcc_slider(
+            id = "month-selected",
+            min = 3,
+            max = 12,
+            marks = Dict(3 => "March", 4 => "April", 5 => "May", 6 => "June", 7 => "July", 8 => "August", 9 => "September", 10 => "October", 11 => "November", 12 => "December"),
+            value = 8,
             )
         end
     end
 end
 
-callback!(app, callid"month-selected.value => my-graph.figure") do selected
+callback!(app, callid"month-selected.value => my-graph.figure") do selected 
+
     Plot(
-        df[df.Month .== selected, :],
-        Layout(
-            title = "Cases Reported Monthly",
-            margin = Dict("l" => 300, "r" => 300),
-            legend = Dict("x" => 1, "y" => 0.7)
-        ),
-        marker = Dict("colors" => ["#EF963B", "#C93277", "#349600", "#EF533B", "#57D4F1"]),
-        textinfo = "label"
+        #how to complete this.
     )
+
 end
 
 
 handler = make_handler(app, debug = true)
-println("App started at localhost:8080")
+println("Come On Madhav ! You can do this!!!")
 HTTP.serve(handler, HTTP.Sockets.localhost, 8080)
 
 
